eba74fe63b6ddc04d8947cd9ac78456dcdac85f7
Luana Batista <97992822+LuanaLB@users.noreply.github.com>
2024-02-01 10:45:18 -0300

    first commit

---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,7 @@
+export function Cart(){
+    return(
+      <div>
+        <h1>Pagina Carinho de compras</h1>
+      </div>
+    )
+  }
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+export function Home(){
+  return(
+    <div>
+      <main className="w-full max-w-7xl px-4 mx-auto bg-gray-300">
+        <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
+          Produtos em alta
+        </h1>
+
+        <div>
+          <section className="w-full">
+            
+          </section>
+        </div>
+      </main>
+    </div>
+  )
+}
